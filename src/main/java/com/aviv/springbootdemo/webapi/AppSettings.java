3fce99172168cbd6e0b1a16a7c212f3042be2688
Aviv Day <avivday@gmail.com>
2022-10-16 02:31:54 +0300

    WIP: starting to add Jwt and security filter

--- src/main/java/com/aviv/springbootdemo/webapi/AppSettings.java
+++ src/main/java/com/aviv/springbootdemo/webapi/AppSettings.java
@@ -10,8 +10,21 @@ public class AppSettings {
     @Value("${app.settings.exceptions.detail}")
     private boolean detailedExceptions;
 
-    @Bean
+    @Value("${app.settings.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${app.settings.jwt.expiration-milliseconds}")
+    private int jwtExpirationInMs;
+
     public boolean showDetailedExceptions() {
         return this.detailedExceptions;
+    }
+
+    public String getJwtSecret() {
+        return this.jwtSecret;
+    }
+
+    public int getJwtExpirationInMs() {
+        return this.jwtExpirationInMs;
     }
 }
